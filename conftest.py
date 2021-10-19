e2569e4b38685d57d6d8b4d0f3ab0f3423168d0a
Nicolay Karlov <karlov.nic@gmail.com>
2021-10-19 17:01:39 +0700

    Closer to the Page Object Model

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='py',
+    parser.addoption('--language', action='store', default='en',
                      help="Choose your language: ru, en, it, fr or another")
 
 
